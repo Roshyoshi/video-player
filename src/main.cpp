98d6f31304501477a7563cbe23e49ca89c36648c
Roshan Hegde <r3hegde@uwaterloo.ca>
2026-01-20 22:36:47 -0500

    test: printing information about test video

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
 
-auto main(int argc, char *argv[]) -> int {
-    std::cout << "hello world";
-    return 0;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+      return -1;
+  }
+
+  AVFormatContext *context = avformat_alloc_context();
+  avformat_open_input(&context, argv[1], NULL, NULL);
+  std::cout << "Format " << context->iformat->long_name << '\n';
+  std::cout << "duration" << context->duration;
 }
